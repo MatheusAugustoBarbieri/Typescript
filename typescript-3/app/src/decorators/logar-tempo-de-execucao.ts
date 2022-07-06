d945e2365a89beecbb35bf4998c806fc577ef763
MatheusAugustoBarbieri <barbiericonta@hotmail.com>
2022-07-06 08:18:24 -0300

    estudos

---
+++ typescript-3/app/src/decorators/logar-tempo-de-execucao.ts
@@ -0,0 +1,52 @@
+export function logarTempoDeExecucao() {
+  return function (
+    // target pode ser constructor ou prototype
+    target: any,
+    // propertyKey da o nome do metodo
+    propertyKey: string,
+    // descriptor tem tudo sobre o metodo
+    descriptor: PropertyDescriptor
+  ) {
+    const metodoOriginal = descriptor.value;
+    // ...args rest operator aceita todos os parametros
+    descriptor.value = function (...args: any[]) {
+      const t1 = performance.now();
+      //chama o metodo original
+      const retorno = metodoOriginal.apply(this, args);
+      const t2 = performance.now();
+      console.log(`${propertyKey}, tempo de excecução: ${(t2 - t1) / 1000}`);
+      retorno;
+    };
+    return descriptor;
+  };
+}
+
+// exemplos do metodo apply() para melhor compreensão
+
+// const person = {
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+
+// const person1 = {
+//   firstName: "Mary",
+//   lastName: "Doe"
+// }
+
+// // This will return "Mary Doe":
+// person.fullName.apply(person1);
+
+// com argumentos
+// const person = {
+//   fullName: function(city, country) {
+//     return this.firstName + " " + this.lastName + "," + city + "," + country;
+//   }
+// }
+
+// const person1 = {
+//   firstName:"John",
+//   lastName: "Doe"
+// }
+
+// person.fullName.apply(person1, ["Oslo", "Norway"]);
